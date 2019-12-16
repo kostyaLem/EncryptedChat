89ea047af55ad476230db46ec6a768515e31357f
k.lemduanov <kostyaLem@yandex.ru>
2019-12-17 01:19:34 +0300

    Top design UI. Look at this !!!

--- EncryptedChat.Client/MainWindow.xaml.cs
+++ EncryptedChat.Client/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Specialized;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EncryptedChat.Client
 {
@@ -25,6 +13,11 @@ namespace EncryptedChat.Client
         {
             InitializeComponent();
             ((INotifyCollectionChanged)MessagesItems.ItemsSource).CollectionChanged += (s, e) => Scroller.ScrollToEnd();
+        }
+
+        private void DockPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.DragMove();
         }
     }
 }
